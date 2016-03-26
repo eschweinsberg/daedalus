1ae942c0f168859784bb89e9c0ebb3b01e8c8abd
Ethan Schweinsberg <ees37@zips.uakron.edu>
2016-03-26 02:19:20 -0400

    created a helper library for the 10DOF module, which includes functions for getting pitch, roll, and heading

--- daedalus_sw_I/src/main.cpp
+++ daedalus_sw_I/src/main.cpp
@@ -1,11 +1,17 @@
 #include "Arduino.h"
+#include "imu/imu.h"
+
+Imu imu;
 
 void setup()
 {
-
+  // Begin communication on the main serial port for debugging
+  Serial.begin(9600);
+  imu.initAll();
 }
 
 void loop()
 {
-
+  imu.debugAllToConsole();
+  delay(2000);
 }
