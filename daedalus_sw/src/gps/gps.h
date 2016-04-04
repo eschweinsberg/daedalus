3effc9bc2874776c26e75bce630a688a03e29c4d
Nana <Nanaanim27@yahoo.com>
2016-04-04 17:55:39 -0400

    GPS code
    Library for GPS

--- daedalus_sw/src/gps/gps.h
+++ daedalus_sw/src/gps/gps.h
@@ -1,1 +1,27 @@
+/**
+ * Property of The University of Akron - Rocket Design Team
+ *
+ * DESCRIPTION: Helper library for simplifying getting values from the Copernicus
+ * II GPS
+ *
+ * AUTHOR:
+ */
+
 #include <copernicus.h>
+
+class Gps
+{
+public:
+  const double ERROR = -1000000;
+
+  void initAll();
+  void debugAllToConsole();
+  double getLatitude();
+  double getLongitude();
+  double getAltitude();
+
+private:
+  CopernicusGPS gps;
+  bool set_fixmode = false;
+
+};
